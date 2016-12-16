6e6da1e2dc16e368bd7e60fd34500c4b21d62aa2
jon <jonathan.lazarini@wipro.com>
2016-12-16 14:01:04 +0100

    boilerplate ready to use

---
+++ src/todo.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jonlazarini on 16/12/16.
+ */
+export class Todo {
+  // gets todoDescription arg from Todos in App.js
+  constructor(description) {
+    this.description = description
+    this.done = false
+  }
+
+
+}
